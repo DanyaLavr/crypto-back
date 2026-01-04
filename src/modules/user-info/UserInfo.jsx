02402ebb5c5daf9a6e011c6e0dc89b8eb470b4e5
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2026-01-04 16:37:31 +0100

    add refresh to logout

--- src/modules/user-info/UserInfo.jsx
+++ src/modules/user-info/UserInfo.jsx
@@ -3,11 +3,13 @@
 import { logoutUser } from "@/lib/redux/user/operations";
 import { selectUser } from "@/lib/redux/user/selectors";
 import Button from "@/shared/buttons/Button";
+import { useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function UserInfo() {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
+  const router = useRouter();
   if (!user) return null;
 
   return (
@@ -17,8 +19,9 @@ export default function UserInfo() {
         color="light"
         background={false}
         className="h-full border-2 py-0 px-9 rounded-2xl"
-        onClick={() => {
-          dispatch(logoutUser());
+        onClick={async () => {
+          await dispatch(logoutUser()).unwrap();
+          router.refresh();
         }}
       >
         Log out
