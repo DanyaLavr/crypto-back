2d090c7b996ca198d0ba4159920cb3e391f1e49e
DanyaLavrovskiy <lavrovskiy.danya@gmail.com>
2026-01-14 13:13:31 +0100

    add underline to next/link

--- src/features/navigation/navigation/Navigation.jsx
+++ src/features/navigation/navigation/Navigation.jsx
@@ -3,19 +3,23 @@ import Link from "next/link";
 import { useSelector } from "react-redux";
 
 import { selectUser } from "@/entities/user/modules/redux/selectors";
+import { usePathname } from "next/navigation";
 
 export default function Navigation() {
   const user = useSelector(selectUser);
+  const pathname = usePathname();
+  const isActiveClass = (href) =>
+    pathname === href ? "p-3 pl-0 underline" : "p-3 pl-0";
   return (
     <nav className="md:text-white font-bold text-2xl lg:text-3xl ">
       <ul className="grid gap-5 justify-items-center md:justify-items-start">
         <li>
-          <Link className="p-3 pl-0" href="/">
+          <Link className={isActiveClass("/")} href="/">
             Home
           </Link>
         </li>
         <li>
-          <Link className="p-3 pl-0" href={`/backpack`}>
+          <Link className={isActiveClass("/backpack")} href={`/backpack`}>
             Backpack
           </Link>
         </li>
